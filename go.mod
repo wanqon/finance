aaa13318679c4b7ada21131b474b5ab1cbefc357
qiongshu <2243395566@qq.com>
2020-04-22 23:40:55 +0800

    graceful exit

--- go.mod
+++ go.mod
@@ -3,9 +3,13 @@ module finance
 go 1.14
 
 require (
-	github.com/lestrrat-go/file-rotatelogs v2.3.0+incompatible // indirect
+	github.com/fastly/go-utils v0.0.0-20180712184237-d95a45783239 // indirect
+	github.com/jehiah/go-strftime v0.0.0-20171201141054-1d33003b3869 // indirect
+	github.com/jonboulle/clockwork v0.1.0 // indirect
+	github.com/lestrrat-go/file-rotatelogs v2.3.0+incompatible
 	github.com/lestrrat-go/strftime v1.0.1 // indirect
 	github.com/pkg/errors v0.9.1 // indirect
 	github.com/robfig/cron/v3 v3.0.0
+	github.com/tebeka/strftime v0.1.4 // indirect
 	go.uber.org/zap v1.14.1
 )
--- main.go
+++ main.go
@@ -1,38 +1,73 @@
 package main
 
 import (
-	"finance/reconciliation"
 	"fmt"
 	"github.com/robfig/cron/v3"
 	"log"
 	"os"
+	"os/signal"
+	"syscall"
+	"time"
 )
 
 func main() {
 
-	a := reconciliation.NewCharge("")
-	a.Run()
 
-	return
+	//a := reconciliation.NewCharge("")
+	//a.Run()
+
+	//return
 
 	loger := cron.PrintfLogger(log.New(os.Stdout,"cron: ", log.LstdFlags))
-	c := cron.New(cron.WithSeconds())
+	c := cron.New(cron.WithSeconds(),cron.WithChain(cron.SkipIfStillRunning(loger)))
 
-	job := cron.NewChain(cron.SkipIfStillRunning(loger)).Then(reconciliation.New("WbpayCharge"))
-	c.AddJob("*/5 * * * * *", job)
+	//job := cron.NewChain(cron.SkipIfStillRunning(loger)).Then(reconciliation.New("WbpayCharge"))
+	//c.AddJob("*/5 * * * * *", job)
 
-	//c.AddFunc("*/5 * * * * *", func() {
-	//	p++
-	//	fmt.Printf("5 seconde start:%d\n", p)
-	//	time.Sleep(7*time.Second)
-	//	fmt.Printf("5 seconde end:%d\n", p)
-	//})
+	p := 0
+
 	c.AddFunc("*/10 * * * * *", func() {
-		fmt.Println("10 seconde once")
+		p++
+		fmt.Printf("5 second start:%d\n", p)
+		for i:=0; i<8; i++ {
+			time.Sleep(time.Second)
+			fmt.Printf("5 second sleep:%d\n", i)
+		}
+		fmt.Printf("5 second end:%d\n", p)
+
+	})
+	c.AddFunc("*/10 * * * * *", func() {
+		fmt.Println("10 second once")
+
 	})
 	c.Start()
+	signalHandle(c)
 
-	select {
+}
 
+func signalHandle(c *cron.Cron)  {
+	ch := make(chan os.Signal, 1)
+	signal.Notify(ch, syscall.SIGINT, syscall.SIGTERM, syscall.SIGUSR2)
+	for {
+		sig := <-ch
+		log.Printf("signal: %v", sig)
+		switch sig {
+		case syscall.SIGINT, syscall.SIGTERM:
+			log.Printf("graceful shutdown")
+			return
+		case syscall.SIGUSR2:
+			ctx := c.Stop()
+			select {
+			case <-ctx.Done():
+				fmt.Println("context was done too quickly immediately")
+			//case <-time.After(750 * time.Millisecond):
+			//	fmt.Println("what")
+			//	// expected, because the job sleeping for 1 second is still running
+			}
+			//time.Sleep(10*time.Second)
+
+			log.Printf("graceful reload")
+			return
+		}
 	}
 }
